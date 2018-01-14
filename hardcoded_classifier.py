98cf70159cf500e15e75ba639f7c5fb326cd3e61
James Palmer <jms.plmr@gmail.com>
2018-01-13 20:59:29 -0700

    Finished Prove01

--- hardcoded_classifier.py
+++ hardcoded_classifier.py
@@ -1,7 +1,25 @@
+# CS450 Prove01
+# James Palmer
+
 import numpy as np
 from sklearn import datasets
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+
+
+class HardcodedClassifier:
+    def __init__(self):
+        pass
+    
+    def fit(self, data, target):
+        return HardcodedClassifier()
+    
+    def predict(self, data_test):
+        return np.zeros((data_test.shape[0],), dtype = int)
+    
+    def score(self, x_test, y_test, sample_weight=None):
+        return accuracy_score(y_test, self.predict(x_test), sample_weight = sample_weight)
 
 
 def main():
@@ -9,20 +27,26 @@ def main():
     
     iris_data = iris.data
     iris_target = iris.target
-    # iris_target_names = iris.target_names
     
     data_train, data_test, target_train, target_test = \
         train_test_split(iris_data, iris_target, test_size = .3)
-    
-    classifier = GaussianNB()
-    model = classifier.fit(data_train, target_train)
-    targets_predicted = model.predict(data_test)
-    
-    print(targets_predicted)
-    
-    number_correct = np.sum(targets_predicted == target_test)
-    total_tests = len(target_test)
-    print(number_correct / total_tests)
+
+    model_iris_gaussian_nb(data_test, data_train, target_test, target_train)
+    model_iris_hardcoded(data_test, data_train, target_test, target_train)
+
+
+def model_iris_hardcoded(data_test, data_train, target_test, target_train):
+    hc_classifier = HardcodedClassifier()
+    hc_classifier.fit(data_train, target_train)
+    hcc_score = hc_classifier.score(data_test, target_test)
+    print("(Hardcoded) Predicted accuracy: {:.02f}%".format(hcc_score * 100))
+
+
+def model_iris_gaussian_nb(data_test, data_train, target_test, target_train):
+    gnb_classifier = GaussianNB()
+    gnb_classifier.fit(data_train, target_train)
+    gnb_score = gnb_classifier.score(data_test, target_test)
+    print("(GaussianNB) Predicted accuracy: {:.02f}%".format(gnb_score * 100))
 
 
 if __name__ == '__main__':
